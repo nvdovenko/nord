f8b930064be9cf504c0ca4ceea58cfe7099dc6cc
Anastasiia Vdovenko <nastyalukovnikova@gmail.com>
2025-06-03 15:43:12 +0300

    Add changes is css for shopping bag

--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -52,44 +52,64 @@ const Cart: React.FC = () => {
     return <p className="empty-cart">Your cart is empty.</p>;
   }
 
+  const totalPrice = cartProducts
+    .reduce((sum, p) => sum + p.totalPrice, 0)
+    .toFixed(2);
+
   return (
-    <div className="cart-container">
-      <h2 className="cart-header">SHOPPING CART</h2>
-      <div className="cart-list">
-        {cartProducts.map((product) => (
-          <div key={product.id} className="cart-item">
-            <img
-              src={product.image}
-              alt={product.name}
-              className="cart-image"
-            />
-            <div className="cart-details">
-              <p className="cart-name">{product.name}</p>
-              <p className="cart-price">${product.totalPrice.toFixed(2)}</p>
+    <div className="catalog">
+      <div className="header-container">SHOPPING BAG</div>
+      <div className="cart-body">
+        <div className="cart-list">
+          {cartProducts.map((product) => (
+            <div key={product.id} className="cart-item">
+              <img
+                src={product.image}
+                alt={product.name}
+                className="cart-image"
+              />
+              <div className="cart-details">
+                <p className="cart-name">{product.name}</p>
+                <p className="cart-price">${product.totalPrice.toFixed(2)}</p>
 
-              <quantity-selector
-                value={product.quantity}
-                onQuantityChange={(e: CustomEvent) =>
-                  changeQuantity(
-                    product.id,
-                    (e as CustomEvent).detail.value - product.quantity
-                  )
-                }
-                onQuantityZero={() =>
-                  changeQuantity(product.id, -product.quantity)
-                }
-              ></quantity-selector>
+                <quantity-selector
+                  value={product.quantity}
+                  onQuantityChange={(e: CustomEvent) =>
+                    changeQuantity(
+                      product.id,
+                      (e as CustomEvent).detail.value - product.quantity
+                    )
+                  }
+                  onQuantityZero={() =>
+                    changeQuantity(product.id, -product.quantity)
+                  }
+                ></quantity-selector>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
-
-      <div className="cart-summary">
-        <div className="total-price">
-          TOTAL PRICE: $
-          {cartProducts.reduce((sum, p) => sum + p.totalPrice, 0).toFixed(2)}
+          ))}
         </div>
-        <basic-button label="BUY NOW"> </basic-button>
+
+        <div className="cart-summary">
+          <div className="horizontal-line">
+            <div className="cart-small-title">Order price </div>
+            <div className="cart-small-price">${totalPrice}</div>
+          </div>
+          <div className="horizontal-line">
+            <div className="cart-small-title">Delivery </div>
+            <div className="cart-small-price"> 0</div>
+          </div>
+          <div className="horizontal-line">
+            <div className="cart-small-title">Discount </div>
+            <div className="cart-small-price"> 0</div>
+          </div>
+          <div className="horizontal-line">
+            <div className="total-price">TOTAL </div>
+            <div className="total-price">${totalPrice}</div>
+          </div>
+          <div className="taxes-included">Taxes included </div>
+
+          <basic-button label="BUY NOW"> </basic-button>
+        </div>
       </div>
     </div>
   );
